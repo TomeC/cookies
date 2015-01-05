595f2790b2490832545687213776a522d776476f
Benjamin <1435452780@qq.com>
2015-01-06 00:06:52 +0800

    handle

---
+++ cpp rumatation/handle/Handle.cpp
@@ -0,0 +1,74 @@
+﻿#include "Handle.h"
+
+Handle::Handle():m_up(0) { }
+
+Handle::Handle(int x, int y):m_up(new UPoint(x, y)) { }
+
+Handle::Handle(const Point &p):m_up(new UPoint(p)) { }
+
+Handle::Handle(const Handle &h):m_up(h.m_up) { ++h.m_up->u; }
+
+Handle::~Handle()
+{
+    if (--m_up->u == 0)
+    {
+        delete m_up;
+    }
+}
+
+Handle& Handle::operator=(const Handle &h)
+{
+    ++h.m_up->u;
+    if (--m_up->u == 0)
+    {
+        delete m_up;
+    }
+    m_up = h.m_up;
+    return *this;
+}
+
+int Handle::x() const
+{
+    return m_up->m_p.x();
+}
+
+int Handle::y() const
+{
+    return m_up->m_p.y();
+}
+
+/*
+//指针语义
+Handle& Handle::x(int x)
+{
+    m_up->m_p.x(x);
+    return *this;
+}
+Handle& Handle::y(int y)
+{
+    m_up->m_p.y(y);
+    return *this;
+}
+*/
+//值语义
+Handle& Handle::x(int x)
+{
+    if (m_up->u != 1)
+    {
+        --m_up->u;
+        m_up = new UPoint(m_up->m_p);
+    }
+    m_up->m_p.x(x);
+    return *this;
+}
+
+Handle& Handle::y(int y)
+{
+    if (m_up->u != 1)
+    {
+        --m_up->u;
+        m_up = new UPoint(m_up->m_p);
+    }
+    m_up->m_p.y(y);
+    return *this;
+}
---
+++ cpp rumatation/handle/Handle.h
@@ -0,0 +1,48 @@
+﻿#ifndef _HANDLE_H_
+#define _HANDLE_H_
+class Handle;
+class Point
+{
+public:
+    Point():m_x(0), m_y(0) { }
+    Point(int x, int y):m_x(x), m_y(y) { }
+    Point(const Point &p):m_x(p.m_x), m_y(p.m_y) { }
+    int x() const { return m_x; }
+    int y() const { return m_y; }
+    Point& x(int ix) { m_x = ix; return *this; }
+    Point& y(int iy) { m_y = iy; return *this; }
+private:
+    int m_x;
+    int m_y;
+};
+
+class UPoint
+{
+    friend class Handle;
+private:
+    UPoint():u(1) { }
+    UPoint(const Point &up):m_p(up), u(1) { }
+    UPoint(int x, int y):m_p(x, y), u(1) {}
+private:
+    Point m_p;
+    int u;
+};
+
+class Handle
+{
+public:
+    Handle();
+    Handle(int, int);
+    Handle(const Point &);
+    Handle(const Handle &);
+    Handle& operator=(const Handle &);
+    ~Handle();
+    //call function
+    int x() const;
+    int y() const;
+    Handle& x(int);
+    Handle& y(int);
+private:
+    UPoint* m_up;
+};
+#endif
